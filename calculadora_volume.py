ed42d1f3c43c58188b977293794ccc1470666683
Tuninho_kjr <152266566+Yagami-Murakami@users.noreply.github.com>
2025-12-12 21:27:02 -0300

    Add volume calculator for a rectangular box

---
+++ calculadora_volume.py
@@ -0,0 +1,10 @@
+comprimento = 12  # cm
+largura = 14      # cm
+altura = 20       # cm
+
+volume = comprimento * largura * altura
+
+print("Comprimento:", comprimento, "cm")
+print("Largura:", largura, "cm")
+print("Altura:", altura, "cm")
+print("Volume da caixa:", volume, "cm³")
